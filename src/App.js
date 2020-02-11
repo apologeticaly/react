72f551df88d9b526960db4f3ecef089443609b4d
Constantino <muheec@gmail.com>
2020-02-11 11:16:19 -0800

    header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import PageHeader from './PageHeader';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <p>This is a test of the paragraph in the JSX world.</p>
-        </header>
+    <div className='App'>
+    <PageHeader />  
     </div>
   );
 }
---
+++ src/PageHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PageHeader.css'
+
+function PageHeader() {
+  return (
+    <div className="PageHeader">
+    <h1>Top 10 Pokemon</h1>
+    </div>
+  )
+}
+
+export default PageHeader
